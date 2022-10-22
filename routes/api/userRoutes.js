1a05de7800ba8c01700e6ef2e6e740321c6fc1c1
Phinjock Sherpa <phinjock@gmail.com>
2022-10-21 20:36:14 -0700

    updater userRoute.js WIP Thoughts controllers and routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const {
+  getUser,
+  getSingleUser,
+  createUser,
+  deleteUser,
+  updateUser,
+} = require("../../controllers/userController");
+
+//api/users
+router.route("/").get(getUser).post(createUser);
+
+//api/users/:userId
+router.route("/:userId").get(getSingleUser).delete(deleteUser).put(updateUser);
+
+module.exports = router;
