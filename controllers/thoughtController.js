849e69af659c9d59151c8a3fe869af17faf055ed
Phinjock Sherpa <phinjock@gmail.com>
2022-10-24 10:51:59 -0700

    working on fixing the 400status on thought controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -23,10 +23,23 @@ module.exports = {
   },
   createThought(req, res) {
     Thought.create(req.body)
-      .then((thought) => res.json(thought))
+      .then((thought) => {
+        return User.findOneAndUpdate(
+          { _id: req.body.userId },
+          { $push: { thoughts: thought._id } },
+          { new: true }
+        );
+      })
+      .then((thought) => {
+        !user
+          ? res.status(404).json({
+              message: "Thought created but no user with this ID",
+            })
+          : res.json(thought);
+      })
       .catch((err) => {
         console.log(err);
-        return res.status(500).json(err);
+        res.status(500).json(err);
       });
   },
 
